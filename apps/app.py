27d8f3f80f41c92c345b0a513c9050b631a5914b
Ubuntu <ubuntu@ip-172-31-15-194.ap-south-1.compute.internal>
2026-08-10 16:16:20 +0000

    Day 13 - Fix container runtime and application port

--- apps/app.py
+++ apps/app.py
@@ -7,4 +7,4 @@ def home():
     return "<h1>Enterprise DevSecOps Pipeline</h1><p>Flask Application Running Successfully!</p>"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=5000)
